8f047a653e958bd9ae399195f138df08dc336578
MinseokGO <96585636+MinseokGo@users.noreply.github.com>
2023-04-11 20:53:39 +0900

    asd

---
+++ src/main/java/ch02/Order.java
@@ -0,0 +1,9 @@
+package ch02;
+
+public class Order {
+    int orderId;    //주문 번호
+    String buyerId; //회원 ID
+    String sellerId; //판매자 ID
+    int productId;    //상품 번호
+    String orderDate;   //주문 일자
+}
