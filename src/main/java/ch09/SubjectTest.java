f27987aa883d9b52927755ac64e2614013dfb9e2
MinseokGO <96585636+MinseokGo@users.noreply.github.com>
2023-04-13 21:10:02 +0900

    asd

---
+++ src/main/java/ch09/SubjectTest.java
@@ -0,0 +1,11 @@
+package ch09;
+
+public class SubjectTest {
+    public static void main(String[] args) {
+        Student studentGo = new Student(100, "Go");
+        studentGo.setKoreaSubject("국어", 100);
+        studentGo.setMathSubject("수학", 100);
+
+        studentGo.showScoreInfo();
+    }
+}
