f651b1fc2363feae5abb1ea100171f8c5654d01b
MinseokGO <96585636+MinseokGo@users.noreply.github.com>
2023-04-15 22:06:37 +0900

    asd

---
+++ src/main/java/ch16/EmployeeTest.java
@@ -0,0 +1,14 @@
+package ch16;
+
+public class EmployeeTest {
+    public static void main(String[] args) {
+        Employee employeeLee = new Employee();
+        employeeLee.setEmployeeName("다나카");
+
+        Employee employeeGo = new Employee();
+        employeeGo.setEmployeeName("고민석");
+
+        System.out.println(employeeLee.getEmployeeId());
+        System.out.println(employeeGo.getEmployeeId());
+    }
+}
